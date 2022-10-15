de2f136e7c5b721e64c59500a080cf9fe9fd1bf7
Leonardo L Cunha <leonavidareal@gmail.com>
2022-10-15 11:47:05 -0300

    first try

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,49 @@
-/* Desenvolva sua lógica aqui...*/
+function dataInput() {
+
+    const input = document.getElementById("input")
+    const button = document.getElementById("btn-sub")
+    if (input.value) {
+        button.disabled = false
+    } else {
+        button.disabled = true
+    }
+    button.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        button.innerHTML = ""
+
+        const img = document.createElement("img")
+        img.src = "/src/img/icons8-loading-30.png"
+        img.alt = "spinner"
+        img.classList.add("load")
+        button.appendChild(img)
+        dataAPI(input.value, button)
+
+    })
+
+
+
+
+}
+dataInput()
+
+async function dataAPI(name, button) {
+    try {
+        const data = await fetch(`https://api.github.com/users/${name}`)
+        const dataJson = await data.json()
+        if (data.status == 404) {
+            const spanRed = document.getElementById("red")
+            spanRed.classList.add("red-show")
+        }
+        localStorage.setItem("users", JSON.stringify(dataJson))
+        button.innerHTML = ""
+        button.innerText = "Ver perfil do github"
+        if (data.status == 200) {
+            window.location.replace("/pages/profile/index.html")
+        }
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,49 @@
-/* Desenvolva sua lógica aqui...*/
+const users = localStorage.getItem("users")
+let user = JSON.parse(users)
+
+
+function HeadPrifole() {
+
+    const mainHead = document.querySelector(".mainHead")
+
+    mainHead.insertAdjacentHTML("beforeend", `
+    <div class="box-img-text">
+        <img src="${user.avatar_url}" alt="">
+        <div class="box-texts">
+            <h2 class="text-2">${user.name}</h2>
+            <p class="texts-form">${user.bio}</p>
+        </div>
+    </div>
+    <div class="box-btns">
+        <button class="btn-mail">Email</button>
+        <button class="btn-swt">Trocar de usuário</button>
+    </div>
+    `)
+    const buttonSwt = document.querySelector(".btn-swt")
+    buttonSwt.addEventListener("click", () => {
+        window.location.replace("/pages/home/index.html")
+    })
+}
+HeadPrifole()
+
+
+async function mainProfile() {
+    const data = await fetch(`https://api.github.com/users/${user.login}/repos`)
+    const dataJson = await data.json()
+    console.log(dataJson)
+    const ul = document.querySelector(".card-container")
+    dataJson.forEach(element => {
+        ul.insertAdjacentHTML("beforeend", `
+        <li class="card">
+        <h2 class="text-1">${element.name}</h2>
+        <p class="text-grey">${element.description}</p>
+        <div class="links">
+          <a target ="_blanck" href="${element.html_url}">Repositorio</a>
+          <a target ="_blanck" href="${element.homepage}">Demo</a>
+        </div>
+      </li>
+        `)
+    });
+}
+
+mainProfile()
